fbe45553d35e3c1f1e476c5d1f520e9b980e68de
SbabalweRoux <sbaviaroux@gmail.com>
2024-03-19 13:37:16 +0200

    Fixed the bug and removed some html code for requi

--- scripts.js
+++ scripts.js
@@ -27,35 +27,39 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+// let status = selector(status);
+// let reserve = selector(reserve);
+// let checkout = selector(checkout);
+// let checkin = selector(checkin);
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+// checkin[0].color = ''
+// status[0].style.color = STATUS_MAP.shelf.color
+// reserve[0] = STATUS_MAP.shelf.canReserve ? 'enabled' 
+// checkout[0] = STATUS_MAP.shelf.canCheckout ? 'enabled' 
+// checkin[0] = STATUS_MAP.shelf.canCheckIn ? 'disabled'
 
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+// checkin[1].color = ''
+// status[1].style.color = STATUS_MAP.reserved.color
+// reserve[1] = STATUS_MAP.reserved.canReserve ? 'disabled'
+// checkout[1] = STATUS_MAP.reserved.canCheckout ? 'enabled' 
+// checkin[1] = STATUS_MAP.reserved.canCheckIn ?  'disabled'
 
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+// checkin[2].color = ''
+// status[2].style.color = STATUS_MAP.overdue.color
+// reserve[2] = STATUS_MAP.overdue.canReserve ?  'disabled'
+// checkout[2] = STATUS_MAP.overdue.canCheckout ?  'disabled'
+// checkin[2] = STATUS_MAP.overdue.canCheckIn ? 'enabled' 
 
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+const books = document.querySelectorAll("[id^='book']");
+books.forEach((book, index) => {
+    const status = book.querySelector('.status');
+    const reserve = book.querySelector('.reserve');
+    const checkout = book.querySelector('.checkout');
+    const checkin = book.querySelector('.checkin');
+
+    status.style.color = STATUS_MAP[status.innerText.trim()].color;
+    reserve.disabled = !STATUS_MAP[status.innerText.trim()].canReserve;
+    checkout.disabled = !STATUS_MAP[status.innerText.trim()].canCheckout;
+    checkin.disabled = !STATUS_MAP[status.innerText.trim()].canCheckIn;
+});
